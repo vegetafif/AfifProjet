d980abc71c529ee911b99f33220c6d1e72ba16bc
KT\BenothmaneZ <Zied.Benothmane@kantarmedia.com>
2019-06-16 13:11:17 +0200

    test et creation des classes

--- AfifCar/src/launch/Launch.java
+++ AfifCar/src/launch/Launch.java
@@ -1,10 +1,23 @@
 package launch;
 
+import voitures.voiture;
+
 public class Launch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String modele = "peugeot";
+		String marque = "206";
+		String couleur = "rouge ";
+		double km = 150000;
+		String carburant = "essence";
+		int age = 12;
+		
+		voiture voit = new voiture(modele, marque, couleur, km, carburant, age);		
+		System.out.println(voit);
+		voit.setAge(15);
+		System.out.println(voit);
+		
 	}
 
 }
